46cc5624e9691938640c10aa10ae1582aa522fba
Jason Willems <jason@willems.ca>
2019-05-25 18:53:44 -0700

    DRY

--- main.scala
+++ main.scala
@@ -22,63 +22,71 @@ import scala.util.Try
 */
 
 final case class Subtitle(index: Int, timestamp: String, text: List[String])
+final case class Timestamp(startTime: String, endTime: String) {
+  override def toString = s"$startTime --> $endTime"
+}
 
 object MyApp extends App {
 
-  val targetOffset   = Try(Duration(args(0))).getOrElse({ println("Invalid invocation. Run as sbt run \"-18.1 seconds\" subtitles.srt") ; sys.exit })//-18.1 seconds
-  val filePath       = "/Users/jwillems/Repos/srt_shift/sample.srt" 
-  val outputFilePath = "/Users/jwillems/Repos/srt_shift/sample.srt-NEW"
+  val usage = """
+    |> sbt
+    |> run \"-18.1 seconds\" /path/to/subtitles.srt""".stripMargin
 
-  def splitBySeparator[T]( l: List[T], sep: T): List[List[T]] = l.span(_ != sep) match {
-    case (hd, _ :: tl) => hd :: splitBySeparator(tl, sep)
-    case (hd, _)       => List(hd)
-  }
-
-  def durationToTimestamp(d: Duration): String = {
-    val mil = "%03d".format(d.toMillis % 1000)
-    val sec = "%02d".format((d.toMillis / 1000) % 60)
-    val min = "%02d".format((d.toMillis / 1000 / 60) % 60)
-    val hr  = ((d.toMillis / 1000 / 60 / 60) % 60).toString.reverse.padTo(2, '0').reverse
-
-    s"$hr:$min:$sec,$mil"
-  }
+  val targetOffset   = Try(Duration(args(0))).getOrElse({ println("Invalid invocation. Run as $usage") ; sys.exit })
+  val filePath       = Try(args(1)).getOrElse({ println("Invalid invocation. Run as $usage") ; sys.exit })
+  val outputFilePath = s"${filePath}-NEW"
 
   val fileLines      = File(filePath).lines
   val subtitleGroups = splitBySeparator(fileLines.toList, "")
 
+
+  def splitBySeparator[T](l: List[T], sep: T): List[List[T]] = l.span(_ != sep) match {
+    case (hd, _ :: tl) => hd :: splitBySeparator(tl, sep)
+    case (hd, _)       => List(hd)
+  }
+
+  def millisecondsToTimestamp(d: Long): String = {
+    val mil = "%03d".format(d % 1000)
+    val sec = "%02d".format((d / 1000) % 60)
+    val min = "%02d".format((d / 1000 / 60) % 60)
+    val hr  = ((d / 1000 / 60 / 60) % 60).toString.reverse.padTo(2, '0').reverse
+    
+    s"$hr:$min:$sec,$mil"
+  }
+
+  def extractTimestampsAsMillis(timestamp: String): List[Long] =
+    timestamp
+      .split(" --> ")
+      .map(time =>
+        time    
+          .split("[:,]")
+          .zip(Seq("hours", "minutes", "seconds", "millis"))
+          .map(x => Duration(s"${x._1} ${x._2}"))
+          .map(_.toMillis)
+          .foldLeft(0L)((acc, el) => acc + el)
+      )
+      .toList
+
+
   val subtitleBlocks = for {
-    subGroup          <- subtitleGroups
-    index             <- subGroup
+    subGroup        <- subtitleGroups
+    index           <- subGroup
                           .headOption
                           .map(_.replaceAll("[^0-9]", "").toInt)
-    timeStamp         <- subGroup
+    timeStamp       <- subGroup
                           .drop(1)
                           .headOption
-    
-    startTime         = timeStamp
-                          .split(" --> ")(0)
-                          .split("[:,]")
-                          .zip(Seq("hours", "minutes", "seconds", "millis"))
-                          .map(x => Duration(s"${x._1} ${x._2}"))
-                          .map(_.toMillis)
-                          .foldLeft(0L)((acc, el) => acc + el)
-    
-    shiftedStartTime = durationToTimestamp(Duration(s"${startTime + targetOffset.toMillis} millis"))
-    
-    endTime          = timeStamp
-                          .split(" --> ")(1)
-                          .split("[:,]")
-                          .zip(Seq("hours", "minutes", "seconds", "millis"))
-                          .map(x => Duration(s"${x._1} ${x._2}"))
-                          .map(_.toMillis)
-                          .foldLeft(0L)((acc, el) => acc + el)
-    
-    shiftedEndTime   = durationToTimestamp(Duration(s"${endTime + targetOffset.toMillis} millis"))
-    
-    msgs             =  subGroup.drop(2)
-  } yield (Subtitle.apply _).tupled((index, s"$shiftedStartTime --> $shiftedEndTime", msgs))
+  
+    ts               = extractTimestampsAsMillis(timeStamp)
+    shiftedStartTime = millisecondsToTimestamp(ts(0) + targetOffset.toMillis)
+    shiftedEndTime   = millisecondsToTimestamp(ts(1) + targetOffset.toMillis)
+    newTimestamp     = Timestamp(shiftedStartTime, shiftedEndTime)
 
-  val shiftedSubtitleBlocks = subtitleBlocks.map(x => s"${x.index}\n${x.timestamp}\n${x.text.mkString("\n")}\n")
+    msgs             =  subGroup.drop(2)
+  } yield (Subtitle.apply _).tupled((index, newTimestamp.toString, msgs))
+
+  val shiftedSubtitleBlocks = subtitleBlocks
+                                .map(x => s"${x.index}\n${x.timestamp}\n${x.text.mkString("\n")}\n")
 
   scala.reflect.io.File(outputFilePath).writeAll(shiftedSubtitleBlocks.mkString("\n"))
 }
